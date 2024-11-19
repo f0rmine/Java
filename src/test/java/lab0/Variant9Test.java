967ac463edd7fe3e48e690948dbd31eeb10d0f15
f0rmine <gerbanov95@gmail.com>
2024-11-20 01:48:13 +0200

    refactor comments and added more tasks for lab0

--- src/test/java/lab0/Variant9Test.java
+++ src/test/java/lab0/Variant9Test.java
@@ -67,27 +67,27 @@ public class Variant9Test {
         assertEquals(new Variant9().arrayTask(new int[]{7, 8, 9, 10}), Arrays.asList(10, 8)); // Парні числа в зворотньому порядку
         assertEquals(new Variant9().arrayTask(new int[]{1, 3, 5, 7}), Arrays.asList()); // Немає парних
     }
-
-    @Test
-    public void testMatrixTask() {
-        int[][] matrix = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        assertEquals(new Variant9().matrixTask(matrix, 1), new int[]{1, 4, 7}); // перший стовбець
-        assertEquals(new Variant9().matrixTask(matrix, 2), new int[]{2, 5, 8}); // другий стовбець
-        assertEquals(new Variant9().matrixTask(matrix, 3), new int[]{3, 6, 9}); // третій стовбець
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testMatrixTaskInvalid() {
-        int[][] matrix = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        new Variant9().matrixTask(matrix, 4); // Should throw exception for K > N
-    }
+//
+//    @Test
+//    public void testMatrixTask() {
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        assertEquals(new Variant9().matrixTask(matrix, 1), new int[]{1, 4, 7}); // перший стовбець
+//        assertEquals(new Variant9().matrixTask(matrix, 2), new int[]{2, 5, 8}); // другий стовбець
+//        assertEquals(new Variant9().matrixTask(matrix, 3), new int[]{3, 6, 9}); // третій стовбець
+//    }
+//
+//    @Test(expectedExceptions = IllegalArgumentException.class)
+//    public void testMatrixTaskInvalid() {
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        new Variant9().matrixTask(matrix, 4); // Should throw exception for K > N
+//    }
 
 }
